c5f0a83357aa660bfb054f646111d588217f7076
James Hines <jhines@signetaccel.com>
2016-01-19 11:26:10 -0500

    Adds a hero interface that has properties for id and number

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,11 @@ import {Component} from 'angular2/core';
   template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
 })
 
+interface Hero {
+  id: number;
+  name: string;
+}
+
 export class AppComponent {
   public title = 'Tour of Heroes';
   public hero = 'Windstrom';
