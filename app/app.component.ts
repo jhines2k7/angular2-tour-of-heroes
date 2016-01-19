bc7e31e5cdda5f140ef20394618bed7b0bcbbf80
James Hines <jhines@signetaccel.com>
2016-01-19 11:45:19 -0500

    Moves the interface declaration above the component declaration to fix compilation errors

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,25 @@
 import {Component} from 'angular2/core';
 
-@Component({
-  selector: 'my-app',
-  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
-})
-
 interface Hero {
   id: number;
   name: string;
 }
 
+@Component({
+  selector: 'my-app',
+  template:
+  `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label>{{hero.name}}</div>
+  `
+})
+
 export class AppComponent {
   public title = 'Tour of Heroes';
-  public hero = 'Windstrom';
+  public hero: Hero = {
+    id: 1,
+    name: 'Windstrom'
+  }
 }
